fd7a5e749fcce16c1c11b1475978c450deeceb1a
Ancoromotch <32382453+Ancoromotch@users.noreply.github.com>
2017-12-23 11:36:31 +0900

    Update prepare.sh
    パスワード修正

--- prepare.sh
+++ prepare.sh
@@ -40,8 +40,8 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y hostapd bridge-utils git
 sudo systemctl stop hostapd
-#echo "mysql-server-5.7 mysql-server/root_password passpass" | debconf-set-selections
-#echo "mysql-server-5.7 mysql-server/root_password_again password passpass" | debconf-set-selections
+#echo "mysql-server-5.7 mysql-server/root_password P@ssw0rd" | debconf-set-selections
+#echo "mysql-server-5.7 mysql-server/root_password_again password P@ssw0rd" | debconf-set-selections
 #sudo apt-get -y install mysql-server-5.7
 #sudo systemctl enable mysql
 
